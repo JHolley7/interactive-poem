b40a07483bd0a5e4efbe42be39c3277e479e2645
JHolley7 <jariah.holley05@gmail.com>
2026-07-21 00:11:12 +0000

    updated code?

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@
 
 // Define Random Number Function
 function randomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.random() * (max - min) + min;
 }
 
 // Define divs variable as all divs inside grid container
@@ -14,7 +14,7 @@ function randomize() {
 
     // just the grid divs
     divs.forEach((function (div) {
-        let scale = randomNumber(.5, 1.2);
+        let scale = randomNumber(0.5, 1.2);
         let translateX = randomNumber(0, 50);
         let translateY = randomNumber(0, 20);
         let rotate = randomNumber(0, 360);
@@ -22,13 +22,15 @@ function randomize() {
         // 50% of the time
         if (Math.random() > 0.5) {
             scale = 1; // don't affect scale
-            translateX = randomNumber(0, 80); 
-            translateY = randomNumber(0, 80); 
+            translateX = randomNumber(0, 80);
+            translateY = randomNumber(0, 80);
         }
 
-        div.style.transform = `scale(${scale}) translate(${translateX}%, ${translateY}%) rotate(${rotate}deg);`;
+        div.style.transform = `scale(${scale}) translate(${translateX}%, ${translateY}%) rotate(${rotate}deg)`;
     })
 }
 
-//add event listener to call randomize function
-document.addEventListener("click", randomize);
+// add click listeners to each word so the words are clickable
+divs.forEach(function (div) {
+    div.addEventListener("click", randomize);
+});
